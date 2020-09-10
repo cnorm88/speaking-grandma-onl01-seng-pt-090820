63d6f4ddd027032b20aa8081d891efc6d43e1db2
Crystal Norman <cnorm88@gmail.com>
2020-09-10 19:36:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@
 
 def speak_to_grandma(phrase)
 
-  if phrase == phrase.uppercase
+  if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
